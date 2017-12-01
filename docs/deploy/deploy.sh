72ccfa78445ff883621b3b78c4286aa615bd1e18
haiqingzheng <leo.zheng@hichengdai.com>
2017-12-01 16:11:49 +0800

    切换研发环境

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -1,16 +1,20 @@
 
 #!/usr/bin/expect
 
-set tomcat_path "/mnt/www/coin/tomcat_xn_ctq"
+set tomcat_path "/mnt/www/coin/tomcat_bc_ctq"
 set war_name "bc-ctq"
-set host_ip "47.90.102.163"
+set host_ip "47.96.161.183"
+set host_pwd "cdkjqwertyQ01"
 
 spawn ssh root@${host_ip}
 expect "*password:"
-send "Bei123coin\r"
+send "${host_pwd}\r"
 #关闭tomcat
 expect "*#"
 send "${tomcat_path}/bin/shutdown.sh\r"
+
+expect "*#"
+send "ps -efww|grep -w ${tomcat_path}|grep -v grep|cut -c 9-15|xargs kill -9\r"
 
 expect "*#"
 send "rm -rf ${tomcat_path}/webapps/${war_name}\r"
@@ -22,9 +26,9 @@ expect "*#"
 send "exit\r"
 
 #2.传输文件
-spawn scp /Users/haiqingzheng/Desktop/${war_name}.war root@${host_ip}:${tomcat_path}/webapps
+spawn scp ./${war_name}.war root@${host_ip}:${tomcat_path}/webapps
 expect "*password:"
-send "Bei123coin\r"
+send "${host_pwd}\r"
 set timeout 300
 send "exit\r"
 expect eof
@@ -32,7 +36,7 @@ expect eof
 #3.重启tomcat
 spawn ssh root@${host_ip}
 expect "*password:"
-send "Bei123coin\r"
+send "${host_pwd}\r"
 expect "*#"
 send "${tomcat_path}/bin/startup.sh\r"
 
