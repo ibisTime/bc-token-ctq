cee7ff1a49e19bf0791b5c1d10614a1cc1dd1fa0
haiqingzheng <leo.zheng@hichengdai.com>
2018-02-05 11:27:51 +0800

    v210表结构和初始化脚本整理

--- docs/init_data/init_xn_ctq.sql
+++ docs/init_data/init_xn_ctq.sql
@@ -1,3 +1,4 @@
 
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBlockNumber','4432876','code',now(),'ETH下次从哪个区块开始扫描');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBlockNumber','0','code',now(),'ETH下次从哪个区块开始扫描');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curScBlockNumber','0','code',now(),'SC下次从哪个区块开始扫描');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBtcBlockNumber','0','admin',now(),'BTC下次从哪个区块开始扫描');
--- docs/schema/schema_xn_ctq.sql
+++ docs/schema/schema_xn_ctq.sql
@@ -1,6 +1,4 @@
 
-
-DROP TABLE IF EXISTS `tctq_eth_address`;
 CREATE TABLE `tctq_eth_address` (
   `code` varchar(32) NOT NULL,
   `address` char(42) NOT NULL,
@@ -10,7 +8,6 @@ CREATE TABLE `tctq_eth_address` (
   KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tctq_eth_transaction`;
 CREATE TABLE `tctq_eth_transaction` (
   `hash` char(66) NOT NULL COMMENT '交易哈希',
   `nonce` bigint(20) NOT NULL COMMENT '交易次数',
@@ -32,7 +29,6 @@ CREATE TABLE `tctq_eth_transaction` (
   KEY `status_index` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tctq_sc_address`;
 CREATE TABLE `tctq_sc_address` (
   `code` varchar(32) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -42,7 +38,6 @@ CREATE TABLE `tctq_sc_address` (
   KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tctq_sc_transaction`;
 CREATE TABLE `tctq_sc_transaction` (
   `transactionid` varchar(255) NOT NULL COMMENT '交易哈希',
   `confirmationheight` varchar(66) NOT NULL COMMENT '确认区块高度',
@@ -54,6 +49,31 @@ CREATE TABLE `tctq_sc_transaction` (
   `sync_datetime` datetime NOT NULL COMMENT '同步时间',
   `status` varchar(4) NOT NULL COMMENT '状态 0-未推送 1-已推送',
   PRIMARY KEY (`transactionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tctq_btc_address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `address` char(36) NOT NULL COMMENT '地址',
+  `type` varchar(4) NOT NULL,
+  `create_datetime` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `address_index` (`address`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tctq_btc_utxo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `txid` char(64) NOT NULL,
+  `vout` int(11) NOT NULL,
+  `count` decimal(64,0) NOT NULL,
+  `script_pub_key` text NOT NULL,
+  `address` varchar(40) NOT NULL,
+  `sync_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `block_height` int(11) NOT NULL,
+  `status` varchar(4) NOT NULL COMMENT '1-out未推送，2-out已推送，3-in未推送，4-in已推送',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `txid_vout_unique_key` (`txid`,`vout`),
+  KEY `address` (`address`),
+  KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `tsys_config`;
