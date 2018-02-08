b4b1067f2a5939f4598569efec29e073b86334c7
haiqingzheng <leo.zheng@hichengdai.com>
2018-02-08 14:11:37 +0800

    脚本整理

--- docs/init_data/init_xn_ctq.sql
+++ docs/init_data/init_xn_ctq.sql
@@ -1,4 +1,8 @@
 
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBlockNumber','0','code',now(),'ETH下次从哪个区块开始扫描');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curEthBlockNumber','0','code',now(),'ETH下次从哪个区块开始扫描');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curScBlockNumber','0','code',now(),'SC下次从哪个区块开始扫描');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBtcBlockNumber','0','admin',now(),'BTC下次从哪个区块开始扫描');
+
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmEth','0','admin',now(),'ETH需要多少个区块确认');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmSc','0','admin',now(),'SC需要多少个区块确认');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmBtc','0','admin',now(),'BTC需要多少个区块确认');
