12593ca498604d81f66c88f5e311dd129be93809
haiqingzheng <leo.zheng@hichengdai.com>
2018-03-06 21:44:41 +0800

    update

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -41,8 +41,8 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:2.2.0 \n");
-        writer.append("Description:bc-ctq 2nd \n");
+        writer.append("Version:2.2.1 \n");
+        writer.append("Description:bc-ctq 1st \n");
         writer.flush();
     }
 
