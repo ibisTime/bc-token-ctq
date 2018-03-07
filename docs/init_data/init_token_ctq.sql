8751b4c63ddeb99efd61635001d3c165ea5371d2
pyez1158 <pyez1158@163.com>
2018-03-08 00:08:49 +0800

    bug 修复

--- docs/init_data/init_token_ctq.sql
+++ docs/init_data/init_token_ctq.sql
@@ -1,4 +1,4 @@
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('1','contractAddress','0xEb463487e165cF630E108d4740521D3A56910b7c','admin',now(),'智能合约地址');
+INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('1','contractAddress','0xf4b2Ffd295366211484706cA4AcdBE729988c43b','admin',now(),'智能合约地址');
 
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curEthBlockNumber','0','code',now(),'ETH下次从哪个区块开始扫描');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmEth','0','admin',now(),'ETH需要多少个区块确认');
