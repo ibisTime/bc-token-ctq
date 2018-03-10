65212dadac52b830f1901b11837a39f661b24eaa
pyez1158 <pyez1158@163.com>
2018-03-10 13:45:53 +0800

    版本记录

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -41,7 +41,7 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:2.2.0 \n");
+        writer.append("Version:1.0.0_token \n");
         writer.append("Description:bc-ctq 2nd \n");
         writer.flush();
     }
