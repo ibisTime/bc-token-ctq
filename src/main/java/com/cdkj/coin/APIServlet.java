9e053138451f2113890ef438c90e3d72d11b8a13
haiqingzheng <leo.zheng@hichengdai.com>
2018-03-27 20:09:32 +0800

    v230收尾

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:2.3.0 \n");
-        writer.append("Description:bc-ctq 3rd \n");
+        writer.append("Description:bc-ctq 4th \n");
         writer.flush();
     }
 
