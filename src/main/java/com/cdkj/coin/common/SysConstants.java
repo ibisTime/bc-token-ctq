cb328209f27fea48415cf8711f26e7b25b8c4d15
haiqingzheng <leo.zheng@hichengdai.com>
2018-03-27 22:42:54 +0800

    代码整理

--- src/main/java/com/cdkj/coin/common/SysConstants.java
+++ src/main/java/com/cdkj/coin/common/SysConstants.java
@@ -6,10 +6,6 @@ public class SysConstants {
 
     public static final String BLOCK_CONFIRM_ETH = "blockConfirmEth"; // ETH需要多少个区块确认
 
-    public static final String CUR_SC_BLOCK_NUMBER = "curScBlockNumber"; // SC当前扫描至哪个区块
-
-    public static final String BLOCK_CONFIRM_SC = "blockConfirmSc"; // SC需要多少个区块确认
-
     public static final String CUR_BTC_BLOCK_NUMBER = "curBtcBlockNumber"; // BTC当前扫描至哪个区块
 
     public static final String BLOCK_CONFIRM_BTC = "blockConfirmBtc"; // BTC需要多少个区块确认
