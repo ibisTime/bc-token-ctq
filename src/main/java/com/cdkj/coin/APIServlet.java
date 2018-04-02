6b6bd8c717c507f0c9d88c13706d46d8b6b8fab8
haiqingzheng <leo.zheng@hichengdai.com>
2018-04-02 20:23:13 +0800

    tokenv120第二次发包

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -42,7 +42,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:1.2.0 \n");
-        writer.append("Description:bc-ctq token 1st \n");
+        writer.append("Description:bc-ctq token 2nd \n");
         writer.flush();
     }
 
