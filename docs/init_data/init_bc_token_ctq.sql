4401ff208acc6d9b87644e347fcdf72428fc250b
haiqingzheng <leo.zheng@hichengdai.com>
2018-04-02 17:37:28 +0800

    update

--- docs/init_data/init_bc_token_ctq.sql
+++ docs/init_data/init_bc_token_ctq.sql
@@ -6,3 +6,5 @@ INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) 
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmEth','0','admin',now(),'ETH需要多少个区块确认');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmBtc','0','admin',now(),'BTC需要多少个区块确认');
 INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmToken','0','admin',now(),'Token需要多少个区块确认');
+
+INSERT INTO `tctq_token_contract` (`symbol`,`contract_address`,`create_datetime`) VALUES ('OGC','0x3F53B5cCC5b8EeddF837EdbA3948a1Ad51885471',now());
