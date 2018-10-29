9db592dfb7f4f38b587bc721773f7ee1d987a5e0
haiqingzheng <leo.zheng@hichengdai.com>
2018-10-29 15:43:03 +0800

    v194新建分支

--- src/main/java/com/cdkj/coin/APIServlet.java
+++ src/main/java/com/cdkj/coin/APIServlet.java
@@ -41,8 +41,8 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.4.0 \n");
-        writer.append("Description:bc-ctq THA 1st \n");
+        writer.append("Version:1.9.4 \n");
+        writer.append("Description:bc-ctq theia 1st \n");
         writer.flush();
     }
 
