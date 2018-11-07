e1fcee3d4c4b8221ba6a3b40921f38dd7677402f
13912 <1391241655@qq.com>
2018-11-07 21:04:21 +0800

    修改测试环境配置，btc获取最新区块更改

--- src/main/java/com/cdkj/coin/ao/impl/BTCBlockDataService.java
+++ src/main/java/com/cdkj/coin/ao/impl/BTCBlockDataService.java
@@ -2,6 +2,11 @@ package com.cdkj.coin.ao.impl;
 
 import java.util.Map;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
@@ -10,11 +15,6 @@ import com.cdkj.coin.bitcoin.BTCOriginalTx;
 import com.cdkj.coin.bitcoin.BTCTXs;
 import com.cdkj.coin.common.PropertiesUtil;
 import com.cdkj.coin.exception.BizException;
-
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 @Service
 public class BTCBlockDataService {
@@ -110,7 +110,7 @@ public class BTCBlockDataService {
                 return null;
             }
             return com.alibaba.fastjson.JSON.parseObject(jsonStr)
-                .getLong("blockcount");
+                .getJSONObject("info").getLongValue("blocks");
 
         } catch (Exception e) {
 
