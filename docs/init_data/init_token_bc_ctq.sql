f541bf937199b9c365365b5ede3e785e8a896aad
xieyj <>
2019-01-17 15:04:57 +0800

    脚本整理

--- docs/init_data/init_token_bc_ctq.sql
+++ docs/init_data/init_token_bc_ctq.sql
@@ -1,8 +1,14 @@
-
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curEthBlockNumber','0','admin',now(),'ETH下次从哪个区块开始扫描');
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','curBtcBlockNumber','1','admin',now(),'BTC下次从哪个区块开始扫描');
-
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmEth','0','admin',now(),'ETH需要多少个区块确认');
-INSERT INTO `tsys_config`(type, ckey, cvalue, updater, update_datetime, remark) VALUES ('0','blockConfirmBtc','0','admin',now(),'BTC需要多少个区块确认');
+/*
+-- Query: SELECT type,ckey,cvalue,'admin' updater,now() update_datetime,remark FROM xn_ctq.tsys_config
+-- Date: 2019-01-16 13:38
+*/
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','curEthBlockNumber','6961776','admin','2019-01-16 05:38:06','ETH下次从哪个区块开始扫描');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','curBtcBlockNumber','550175','admin','2019-01-16 05:38:06','BTC下次从哪个区块开始扫描');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','curWanBlockNumber','0','admin','2019-01-16 05:38:06','WAN下次从哪个区块开始扫描');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','curWTokenBlockNumber','0','admin','2019-01-16 05:38:06','WAN Token下次从哪个区块开始扫描');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','blockConfirmEth','12','admin','2019-01-16 05:38:06','ETH需要多少个区块确认');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','blockConfirmBtc','0','admin','2019-01-16 05:38:06','BTC需要多少个区块确认');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','blockConfirmWan','0','admin','2019-01-16 05:38:06','WAN需要多少个区块确认');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`) VALUES ('0','blockConfirmWToken','0','admin','2019-01-16 05:38:06','WAN Token需要多少个区块确认');
 
 INSERT INTO `tctq_token_contract` (`symbol`,`contract_address`,`create_datetime`) VALUES ('FMVP','0x68aaefbdd5cb18ff577966cb2679df42e2eba548',now());
--- docs/schema/schema_token_bc_ctq.sql
+++ docs/schema/schema_token_bc_ctq.sql
@@ -1,14 +1,9 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `tctq_eth_address`;
-CREATE TABLE `tctq_eth_address` (
-  `code` varchar(32) NOT NULL,
-  `address` char(42) NOT NULL,
-  `type` varchar(3) NOT NULL,
-  `create_datetime` datetime NOT NULL,
-  PRIMARY KEY (`code`),
-  KEY `address_index` (`address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+-- ----------------------------
+--  Table structure for `tcoin_eth_transaction`
+-- ----------------------------
 DROP TABLE IF EXISTS `tcoin_eth_transaction`;
 CREATE TABLE `tcoin_eth_transaction` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -34,8 +29,11 @@ CREATE TABLE `tcoin_eth_transaction` (
   `s` text COMMENT 's',
   PRIMARY KEY (`id`),
   UNIQUE KEY `hash_UNIQUE` (`hash`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COMMENT='ETH交易记录';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ETH交易记录';
 
+-- ----------------------------
+--  Table structure for `tcoin_token_event`
+-- ----------------------------
 DROP TABLE IF EXISTS `tcoin_token_event`;
 CREATE TABLE `tcoin_token_event` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -46,8 +44,11 @@ CREATE TABLE `tcoin_token_event` (
   `token_log_index` bigint(32) DEFAULT NULL COMMENT 'event_log_index',
   `symbol` varchar(32) NOT NULL COMMENT '币种符号',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='ETHtoken交易event';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ETHtoken交易event';
 
+-- ----------------------------
+--  Table structure for `tctq_btc_address`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_btc_address`;
 CREATE TABLE `tctq_btc_address` (
   `code` varchar(32) NOT NULL,
@@ -55,8 +56,11 @@ CREATE TABLE `tctq_btc_address` (
   `create_datetime` datetime NOT NULL,
   PRIMARY KEY (`code`),
   KEY `address_index` (`address`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Table structure for `tctq_btc_utxo`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_btc_utxo`;
 CREATE TABLE `tctq_btc_utxo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -74,40 +78,78 @@ CREATE TABLE `tctq_btc_utxo` (
   KEY `status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tsys_config`;
-CREATE TABLE `tsys_config` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
-  `type` varchar(32) DEFAULT NULL COMMENT '类型',
-  `ckey` varchar(255) DEFAULT NULL COMMENT 'key',
-  `cvalue` text COMMENT 'value',
-  `updater` varchar(32) DEFAULT NULL COMMENT '更新人',
-  `update_datetime` datetime DEFAULT NULL COMMENT '更新时间',
-  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `tsys_dict`;
-CREATE TABLE `tsys_dict` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号（自增长）',
-  `type` char(1) DEFAULT NULL COMMENT '类型（第一层/第二层）',
-  `parent_key` varchar(32) DEFAULT NULL COMMENT '父key',
-  `dkey` varchar(32) DEFAULT NULL COMMENT 'key',
-  `dvalue` varchar(255) DEFAULT NULL COMMENT '值',
-  `updater` varchar(32) DEFAULT NULL COMMENT '更新人',
-  `update_datetime` datetime DEFAULT NULL COMMENT '更新时间',
-  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`id`)
+-- ----------------------------
+--  Table structure for `tctq_eth_address`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_eth_address`;
+CREATE TABLE `tctq_eth_address` (
+  `code` varchar(32) NOT NULL,
+  `address` char(42) NOT NULL,
+  `type` varchar(3) NOT NULL,
+  `create_datetime` datetime NOT NULL,
+  PRIMARY KEY (`code`),
+  KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tctq_token_contract`;
-CREATE TABLE `tctq_token_contract` (
-  `symbol` varchar(32) NOT NULL COMMENT '币种符号',
-  `contract_address` varchar(255) NOT NULL COMMENT '合约地址',
-  `create_datetime` datetime NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`symbol`),
-  KEY `address_index` (`contract_address`)
+-- ----------------------------
+--  Table structure for `tctq_eth_transaction`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_eth_transaction`;
+CREATE TABLE `tctq_eth_transaction` (
+  `hash` char(66) NOT NULL COMMENT '交易哈希',
+  `nonce` bigint(20) NOT NULL COMMENT '交易次数',
+  `block_hash` varchar(66) NOT NULL COMMENT '区块哈希',
+  `transaction_index` bigint(20) NOT NULL,
+  `from` char(42) NOT NULL COMMENT '转出地址',
+  `to` char(42) NOT NULL COMMENT '转入地址',
+  `value` varchar(30) NOT NULL COMMENT '交易额',
+  `gas_price` varchar(20) NOT NULL COMMENT 'gas价格',
+  `block_create_datetime` datetime NOT NULL COMMENT '区块形成时间',
+  `sync_datetime` datetime NOT NULL COMMENT '同步时间',
+  `status` varchar(4) NOT NULL COMMENT '状态 0-未推送 1-已推送',
+  `gas` bigint(20) NOT NULL COMMENT 'gas使用量',
+  `block_number` varchar(20) NOT NULL,
+  `gas_used` bigint(10) NOT NULL COMMENT 'gas消耗',
+  PRIMARY KEY (`hash`),
+  KEY `from_index` (`from`),
+  KEY `to_index` (`to`),
+  KEY `status_index` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Table structure for `tctq_sc_address`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_sc_address`;
+CREATE TABLE `tctq_sc_address` (
+  `code` varchar(32) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `type` varchar(3) NOT NULL,
+  `create_datetime` datetime NOT NULL,
+  PRIMARY KEY (`code`),
+  KEY `address_index` (`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tctq_sc_transaction`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_sc_transaction`;
+CREATE TABLE `tctq_sc_transaction` (
+  `transactionid` varchar(255) NOT NULL COMMENT '交易哈希',
+  `outputid` varchar(255) NOT NULL COMMENT 'output哈希',
+  `confirmationheight` varchar(66) NOT NULL COMMENT '确认区块高度',
+  `confirmationtimestamp` bigint(20) NOT NULL COMMENT '确认时间',
+  `from` varchar(255) NOT NULL COMMENT '转出地址',
+  `to` varchar(255) NOT NULL COMMENT '转入地址',
+  `value` varchar(30) NOT NULL COMMENT '交易数量',
+  `minerfee` varchar(30) NOT NULL COMMENT '矿工费',
+  `sync_datetime` datetime NOT NULL COMMENT '同步时间',
+  `status` varchar(4) NOT NULL COMMENT '状态 0-未推送 1-已推送',
+  PRIMARY KEY (`transactionid`,`outputid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tctq_token_address`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_token_address`;
 CREATE TABLE `tctq_token_address` (
   `code` varchar(32) NOT NULL COMMENT '编号',
@@ -118,6 +160,52 @@ CREATE TABLE `tctq_token_address` (
   KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Table structure for `tctq_token_contract`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_token_contract`;
+CREATE TABLE `tctq_token_contract` (
+  `symbol` varchar(32) NOT NULL COMMENT '币种符号',
+  `contract_address` varchar(255) NOT NULL COMMENT '合约地址',
+  `create_datetime` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`symbol`),
+  KEY `address_index` (`contract_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tctq_token_transaction`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_token_transaction`;
+CREATE TABLE `tctq_token_transaction` (
+  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `hash` varchar(255) NOT NULL COMMENT '交易哈希',
+  `nonce` bigint(32) NOT NULL COMMENT '交易次数',
+  `block_hash` varchar(255) NOT NULL COMMENT '区块哈希',
+  `transaction_index` bigint(32) NOT NULL COMMENT '交易index',
+  `from` varchar(255) NOT NULL COMMENT '转出地址',
+  `to` varchar(255) NOT NULL COMMENT '转入地址',
+  `value` decimal(64,0) NOT NULL COMMENT '交易额',
+  `input` text COMMENT 'input 输入',
+  `token_from` varchar(255) DEFAULT NULL COMMENT 'token币发起地址',
+  `token_to` varchar(255) DEFAULT NULL COMMENT 'token币接收地址',
+  `token_value` decimal(64,0) DEFAULT NULL COMMENT 'token币数量',
+  `token_log_index` bigint(32) DEFAULT NULL COMMENT 'event_log_index',
+  `block_create_datetime` datetime NOT NULL COMMENT '区块形成时间',
+  `sync_datetime` datetime NOT NULL COMMENT '同步时间',
+  `block_number` bigint(32) NOT NULL COMMENT '区块号',
+  `gas_price` decimal(64,0) NOT NULL COMMENT 'gas价格',
+  `gas_limit` bigint(20) NOT NULL COMMENT 'gas最大使用限制',
+  `gas_used` bigint(20) NOT NULL COMMENT 'gas实际使用量',
+  `status` varchar(4) NOT NULL COMMENT '状态 0-未推送 1-已推送',
+  `symbol` varchar(32) NOT NULL COMMENT '币种符号',
+  PRIMARY KEY (`id`),
+  KEY `to_index` (`hash`,`token_log_index`),
+  KEY `status_index` (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tctq_wan_address`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_wan_address`;
 CREATE TABLE `tctq_wan_address` (
   `code` varchar(32) NOT NULL,
@@ -128,6 +216,9 @@ CREATE TABLE `tctq_wan_address` (
   KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Table structure for `tctq_wan_transaction`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_wan_transaction`;
 CREATE TABLE `tctq_wan_transaction` (
   `hash` char(66) NOT NULL COMMENT '交易哈希',
@@ -150,16 +241,9 @@ CREATE TABLE `tctq_wan_transaction` (
   KEY `status_index` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `tctq_wtoken_contract`;
-CREATE TABLE `tctq_wtoken_contract` (
-  `symbol` varchar(32) NOT NULL COMMENT '币种符号',
-  `contract_address` varchar(255) NOT NULL COMMENT '合约地址',
-  `create_datetime` datetime NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`symbol`),
-  KEY `address_index` (`contract_address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+-- ----------------------------
+--  Table structure for `tctq_wtoken_address`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_wtoken_address`;
 CREATE TABLE `tctq_wtoken_address` (
   `code` varchar(32) NOT NULL COMMENT '编号',
@@ -170,6 +254,21 @@ CREATE TABLE `tctq_wtoken_address` (
   KEY `address_index` (`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+--  Table structure for `tctq_wtoken_contract`
+-- ----------------------------
+DROP TABLE IF EXISTS `tctq_wtoken_contract`;
+CREATE TABLE `tctq_wtoken_contract` (
+  `symbol` varchar(32) NOT NULL COMMENT '币种符号',
+  `contract_address` varchar(255) NOT NULL COMMENT '合约地址',
+  `create_datetime` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`symbol`),
+  KEY `address_index` (`contract_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tctq_wtoken_transaction`
+-- ----------------------------
 DROP TABLE IF EXISTS `tctq_wtoken_transaction`;
 CREATE TABLE `tctq_wtoken_transaction` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -178,7 +277,7 @@ CREATE TABLE `tctq_wtoken_transaction` (
   `block_hash` varchar(255) NOT NULL COMMENT '区块哈希',
   `transaction_index` bigint(32) NOT NULL COMMENT '交易index',
   `from` varchar(255) NOT NULL COMMENT '转出地址',
-  `to` varchar(255)  NOT NULL COMMENT '转入地址',
+  `to` varchar(255) NOT NULL COMMENT '转入地址',
   `value` decimal(64,0) NOT NULL COMMENT '交易额',
   `input` text COMMENT 'input 输入',
   `token_from` varchar(255) DEFAULT NULL COMMENT 'token币发起地址',
@@ -197,3 +296,36 @@ CREATE TABLE `tctq_wtoken_transaction` (
   KEY `to_index` (`hash`,`token_log_index`),
   KEY `status_index` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tsys_config`
+-- ----------------------------
+DROP TABLE IF EXISTS `tsys_config`;
+CREATE TABLE `tsys_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `type` varchar(32) DEFAULT NULL COMMENT '类型',
+  `ckey` varchar(255) DEFAULT NULL COMMENT 'key',
+  `cvalue` text COMMENT 'value',
+  `updater` varchar(32) DEFAULT NULL COMMENT '更新人',
+  `update_datetime` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tsys_dict`
+-- ----------------------------
+DROP TABLE IF EXISTS `tsys_dict`;
+CREATE TABLE `tsys_dict` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号（自增长）',
+  `type` char(1) DEFAULT NULL COMMENT '类型（第一层/第二层）',
+  `parent_key` varchar(32) DEFAULT NULL COMMENT '父key',
+  `dkey` varchar(32) DEFAULT NULL COMMENT 'key',
+  `dvalue` varchar(255) DEFAULT NULL COMMENT '值',
+  `updater` varchar(32) DEFAULT NULL COMMENT '更新人',
+  `update_datetime` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
